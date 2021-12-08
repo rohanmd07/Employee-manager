67904820f11bdb1f9b553ab9eaebf3bc7282351e
rohanmd07 <41102695+rohanmd07@users.noreply.github.com>
2021-12-08 15:18:37 +0530

    Add files via upload

--- src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repo.EmployeeRepo;
 
+@Service
 public class EmployeeService {
 	private final EmployeeRepo employeeRepo;
 	
@@ -30,7 +32,7 @@ public class EmployeeService {
 		return employeeRepo.save(employee);
 	}
 	
-	public Object findEmployeeById(Long id) throws Throwable {
+	public Employee findEmployeeById(Long id) {
 		return employeeRepo.findEmployeeById(id)
 				.orElseThrow(() -> new UserNotFoundException("User by id "+ id +" not found"));
 	}
